bf7b455b162fa70d4307dacf9aaa473e01c96e95
den05032002 <den05032002@gmail.com>
2026-09-11 12:51:38 +0300

    Create task1.cs

---
+++ task1/task1.cs
@@ -0,0 +1,35 @@
+uint n_first = uint.Parse(args[0]);
+uint m_first = uint.Parse(args[1]);
+uint n_second = uint.Parse(args[2]);
+uint m_second = uint.Parse(args[3]);
+
+List<uint> Path(uint n, uint m)
+{
+  List<uint> result = new List<uint>();
+
+  uint arr = 0;
+
+  if (m == 1 || n == 1)
+  {
+    result.Add(1u);
+    return result;
+  }
+
+  while (true)
+  {
+    if (arr < n)
+    {
+      result.Add(arr + 1);
+      arr += m - 1;
+    } else
+    {
+      arr = arr - n;
+      if (arr == 0) break;
+    }
+  }
+
+  return result;
+}
+
+foreach (uint x in Path(n_first, m_first)) Console.Write(x);
+foreach (uint x in Path(n_second, m_second)) Console.Write(x);
